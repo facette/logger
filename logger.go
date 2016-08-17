a8ede8faff82875ceeeb9276f67f273dca27a73e
Marc Falzon <marc@baha.mu>
2016-08-17 11:48:15 +0200

    Refactor package

--- logger.go
+++ logger.go
@@ -1,3 +1,5 @@
+// Package logger is a simple wrapper around log.Logger with usual logging levels "error", "warning", "notice",
+// "info" and "debug".
 package logger
 
 import (
@@ -9,6 +11,14 @@ import (
 	"github.com/mgutz/ansi"
 )
 
+// Logger represents a logger instance.
+type Logger struct {
+	logger  *log.Logger
+	level   int
+	context string
+	out     *os.File
+}
+
 const (
 	_ = iota
 	levelError
@@ -17,17 +27,12 @@ const (
 	levelInfo
 	levelDebug
 
-	defaultLevel string = "info"
+	defaultLevel = "info"
 )
 
 var (
-	logger   *log.Logger
-	logLevel int
-
 	levelLabels map[int]string
-	levelSuffix string
-
-	levelMap = map[string]int{
+	levelMap    = map[string]int{
 		"error":   levelError,
 		"warning": levelWarning,
 		"notice":  levelNotice,
@@ -36,50 +41,40 @@ var (
 	}
 )
 
-// Init initializes the logging system output path and level.
-func Init(logPath, level string) error {
+// NewLogger returns a new Logger instance initialized with the logging system output path and level. If logPath is
+// either empty or "-", logging will be output to os.Stderr. Log messages with severity higher than level will be
+// discarded.
+func NewLogger(logPath, level string) (*Logger, error) {
 	var (
-		logOut *os.File
-		ok     bool
-		err    error
+		err error
+		ok  bool
 	)
 
-	// Set default log level if none provided
-	if level == "" {
-		level = defaultLevel
-	}
+	logger := &Logger{level: levelMap[defaultLevel]}
 
-	logLevel, ok = levelMap[level]
-	if !ok {
-		return ErrInvalidLevel
+	if logger.level, ok = levelMap[level]; !ok {
+		return nil, ErrInvalidLevel
 	}
 
 	if logPath != "" && logPath != "-" {
+		// Set logging output to a file
 		ansi.DisableColors(true)
-		levelSuffix = ":"
 
 		// Create parent folders if needed
 		dirPath, _ := path.Split(logPath)
 
-		err = os.MkdirAll(dirPath, 0755)
-		if err != nil {
-			return err
+		if err = os.MkdirAll(dirPath, 0755); err != nil {
+			return nil, err
 		}
 
-		// Open logging output file
-		logOut, err = os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			return fmt.Errorf("unable to open log file: %s", err)
+		if logger.out, err = os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); err != nil {
+			return nil, err
 		}
-		defer logOut.Close()
 	} else {
-		// Set logging output to 'stderr' and activate color
-		logOut = os.Stderr
+		// Set logging output to stderr
+		logger.out = os.Stderr
 	}
 
-	logger = log.New(logOut, "", log.LstdFlags|log.Lmicroseconds)
-
-	// Set level labels
 	levelLabels = map[int]string{
 		levelError:   ansi.Color("ERROR", "red"),
 		levelWarning: ansi.Color("WARNING", "yellow"),
@@ -88,44 +83,71 @@ func Init(logPath, level string) error {
 		levelDebug:   ansi.Color("DEBUG", "cyan"),
 	}
 
-	return nil
+	logger.logger = log.New(logger.out, "", log.LstdFlags|log.Lmicroseconds)
+
+	return logger, nil
 }
 
-// Logger returns the current logger instance.
-func Logger() *log.Logger {
-	return logger
+// Logger returns the underlying log.Logger instance.
+func (l *Logger) Logger() *log.Logger {
+	return l.logger
+}
+
+// Context clones the Logger instance and sets the context to the provided string.
+func (l *Logger) Context(context string) *Logger {
+	logger := *l
+	logger.context = context
+	return &logger
 }
 
 // Error prints an error message in the logging system.
-func Error(context, format string, v ...interface{}) {
-	printLog(levelError, context, format, v...)
+func (l *Logger) Error(format string, v ...interface{}) *Logger {
+	return l.print(levelError, format, v...)
 }
 
 // Warning prints a warning message in the logging system.
-func Warning(context, format string, v ...interface{}) {
-	printLog(levelWarning, context, format, v...)
+func (l *Logger) Warning(format string, v ...interface{}) *Logger {
+	return l.print(levelWarning, format, v...)
 }
 
 // Notice prints a notice message in the logging system.
-func Notice(context, format string, v ...interface{}) {
-	printLog(levelNotice, context, format, v...)
+func (l *Logger) Notice(format string, v ...interface{}) *Logger {
+	return l.print(levelNotice, format, v...)
 }
 
 // Info prints an information message in the logging system.
-func Info(context, format string, v ...interface{}) {
-	printLog(levelInfo, context, format, v...)
+func (l *Logger) Info(format string, v ...interface{}) *Logger {
+	return l.print(levelInfo, format, v...)
 }
 
 // Debug prints a debug message in the logging system.
-func Debug(context, format string, v ...interface{}) {
-	printLog(levelDebug, context, format, v...)
+func (l *Logger) Debug(format string, v ...interface{}) *Logger {
+	return l.print(levelDebug, format, v...)
 }
 
-// printLog prints a message in the logging system.
-func printLog(level int, context, format string, v ...interface{}) {
-	if level > logLevel {
-		return
+// Close closes the logger output file.
+func (l *Logger) Close() error {
+	if l.out != nil {
+		return l.out.Close()
 	}
 
-	logger.Printf("%s%s %s: %s", levelLabels[level], levelSuffix, context, fmt.Sprintf(format, v...))
+	return nil
+}
+
+func (l *Logger) print(level int, format string, v ...interface{}) *Logger {
+	if level > l.level {
+		return l
+	}
+
+	if l.context != "" {
+		l.logger.Printf(
+			"%s: %s",
+			fmt.Sprintf("%s: %s", levelLabels[level], l.context),
+			fmt.Sprintf(format, v...),
+		)
+	} else {
+		l.logger.Printf("%s: %s", levelLabels[level], fmt.Sprintf(format, v...))
+	}
+
+	return l
 }
